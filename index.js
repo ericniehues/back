cce06689fd198da717226d3719d52459aba0385d
ericniehues <45860873+ericniehues@users.noreply.github.com>
2020-06-11 18:21:53 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $("#header-host").load("../components/header.html");
+  $("#footer-host").load("../components/footer.html");
+  $("#sobre-host").load("../pages/sobre.html");
+  $("#nossos-produtos-host").load("../pages/nossos-produtos.html");
+  $("#comentarios-clientes-host").load("../pages/comentarios-clientes.html");
+  $("#nossos-precos-host").load("../pages/nossos-precos.html");
+});
